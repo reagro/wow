5c594167a47e92b14515f31ee4232b3e7f3b79a2
rhijmans <r.hijmans@gmail.com>
2026-05-11 05:03:13 -0700

    r

--- R/packages.R
+++ R/packages.R
@@ -1,11 +1,33 @@
 
 install_pkgs <- function(x, update=FALSE) {
-	i <- x %in% rownames(utils::installed.packages())
-	if (!all(i)) {
-		utils::install.packages(x[!i], repos="https://cloud.r-project.org")
+
+	installed <- rownames(utils::installed.packages()) 
+	
+	gi <- grepl("/", x)
+	gh <- x[gi]
+	cran <- x[!gi]
+	
+	if (length(cran) > 0) {
+		i <- cran %in% installed
+		if (!all(i)) {
+			utils::install.packages(cran[!i], repos="https://cloud.r-project.org")
+		}
+		if (update) {
+			utils::update.packages(cran[i], repos="https://cloud.r-project.org", ask=FALSE)
+		}
 	}
-	if (update) {
-		utils::update.packages(x[i], repos="https://cloud.r-project.org", ask=FALSE)
+	
+	if (length(gh) > 0) {
+		if (!("remotes" %in% installed)) {
+			utils::install.packages("remotes", repos="https://cloud.r-project.org")
+		}
+		i <- gh %in% installed
+		if (!all(i)) {
+			remotes::install_github(gh[!i])
+		}
+		if (update) {
+			remotes::install_github(gh[i])
+		}
 	}
 	invisible()
 }
