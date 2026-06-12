8101315de2601ace3cad80723692c103be7ef7d9
rhijmans <r.hijmans@gmail.com>
2026-06-12 12:50:16 -0700

    fld

--- R/folders.R
+++ R/folders.R
@@ -1,12 +1,24 @@
 
-create_folders <- function(path) {
-	dir.create(path, FALSE, TRUE)
-	dir.create(file.path(path, "data/raw"), FALSE, TRUE)
-	dir.create(file.path(path, "data/intermediate"), FALSE)
-	dir.create(file.path(path, "data/final"), FALSE)
-	dir.create(file.path(path, "img"), FALSE)
-	dir.create(file.path(path, "results"), FALSE)
-	invisible(dir(path, recursive=TRUE, include.dirs=TRUE, full.names=TRUE))
+create_folders <- function(path, shared_path=path) {
+	if (!dir.exists(path)) {
+		warning(paste("creating path:", path))
+		dir.create(path, FALSE, TRUE)
+	}		
+	if (!dir.exists(shared_path)) {
+		warning(paste("shared path does not exist:", shared_path))	
+	}
+	rawpath <- file.path(path, "data/raw")
+	intpath <- file.path(path, "data/intermediate")
+	finpath <- file.path(path, "data/final")
+	figpath <- file.path(path, "figures")
+	tabpath <- file.path(path, "tables")	
+	dir.create(rawpath, FALSE, TRUE)
+	dir.create(intpath, FALSE)
+	dir.create(finpath, FALSE)
+	dir.create(figpath, FALSE)
+	dir.create(tabpath, FALSE)
+	
+	list(root=path, raw=rawpath, interm=intpath, final=finpath, figs=figpath, tables=tabpath, share=shared_path)
 }	
 
 
--- R/init.R
+++ R/init.R
@@ -1,18 +1,19 @@
 
 init <- function(repo, org="", path=NULL, ...) {
-	prj_path <- NULL
-	if (!is.null(path)) {
-		f <- file.path(file.path(path, "setup.R"))
-	} else {
-		f <- file.path("https://raw.githubusercontent.com", org, repo, "main/setup.R")
+	if (!(exists(WoW) && is.list(WoW))) {		
+		if (!is.null(path)) {
+			f <- file.path(file.path(path, "0_setup.R"))
+		} else {
+			f <- file.path("https://raw.githubusercontent.com", org, repo, "main/0_setup.R")
+		}
+		x <- try(readLines(f))
+		if (inherits(x, "try-error")) {
+			f <- file.path("https://github.com", org, repo, "0_setup.R")
+			stop(paste(f, "does not exist"))
+		} else {
+			fun <- eval(parse(text=x))
+		}
+		fun(...)
 	}
-	x <- try(readLines(f))
-	if (inherits(x, "try-error")) {
-		f <- file.path("https://github.com", org, repo, "setup.R")
-		stop(paste(f, "does not exist"))
-	} else {
-		fun <- eval(parse(text=x))
-	}
-	fun(...)
 }
 
